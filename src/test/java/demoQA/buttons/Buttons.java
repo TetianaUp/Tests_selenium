29bca4dd82b84ffb2990874bee0981d9dd1dfe9a
Tania Stanko <taniastanko@Tanias-Air.lan>
2023-04-13 17:27:18 +0300

    test with buttons

--- src/test/java/demoQA/buttons/Buttons.java
+++ src/test/java/demoQA/buttons/Buttons.java
@@ -2,17 +2,34 @@ package demoQA.buttons;
 
 import demoQA.AbstractTestInit;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Buttons extends AbstractTestInit {
     @Test
-    public void checkButtons(){
+    public void checkButtons() {
         driver.get("https://demoqa.com/links");
-        driver.findElement(By.xpath("//span[text()='Buttons]")).click();
-        String actualURLButtons= driver.getCurrentUrl();
+        driver.findElement(By.xpath("//span[text()='Buttons']")).click();
+        String actualURLButtons = driver.getCurrentUrl();
         Assert.assertEquals(actualURLButtons, "https://demoqa.com/buttons", "URL isn't correct");
 
+        Actions action = new Actions(driver);
+        WebElement doubleClick = driver.findElement(By.xpath("//button[@id='doubleClickBtn']"));
+        action.doubleClick(doubleClick).perform();
 
+        Assert.assertEquals(driver.findElement(By.xpath("//p[@id='doubleClickMessage']")).getText(),
+                "You have done a double click");
+
+        WebElement rightClick = driver.findElement(By.xpath("//button[@id='rightClickBtn']"));
+        action.contextClick(rightClick).perform();
+
+        Assert.assertEquals(driver.findElement(By.xpath("//p[@id='rightClickMessage']")).getText(),
+                "You have done a right click");
+
+        driver.findElement(By.xpath("//button[text()='Click Me']")).click();
+        Assert.assertEquals(driver.findElement(By.xpath("//p[@id='dynamicClickMessage']")).getText(),
+                "You have done a dynamic click");
     }
 }
