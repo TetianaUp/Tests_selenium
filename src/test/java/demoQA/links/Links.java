672ba0b9f42466ac9864098862ec88d344f963de
Tania Stanko <taniastanko@Tanias-MacBook-Air.local>
2023-04-17 15:56:05 +0300

    test with links

--- src/test/java/demoQA/links/Links.java
+++ src/test/java/demoQA/links/Links.java
@@ -15,125 +15,152 @@ public class Links extends AbstractTestInit {
 
     @Test
     public void checkLinks() throws InterruptedException {
-        this.driver.get("https://demoqa.com/elements");
-        scrollToElement(this.driver, this.driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")));
-        this.driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")).click();
-        String actualURLButtons = this.driver.getCurrentUrl();
+        driver.get("https://demoqa.com/elements");
+        scrollToElement(driver, driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")));
+        driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")).click();
+        String actualURLButtons = driver.getCurrentUrl();
+
         Assert.assertEquals(actualURLButtons, "https://demoqa.com/links", "URL isn't correct");
-        this.driver.findElement(By.xpath("//a[@id='simpleLink']")).click();
+
+        driver.findElement(By.xpath("//a[@id='simpleLink']")).click();
         Thread.sleep(2000L);
-        Set<String> windowHandles = this.driver.getWindowHandles();
+        Set<String> windowHandles = driver.getWindowHandles();
         Iterator<String> iterator = windowHandles.iterator();
         String mainWindowHandle = (String) iterator.next();
         String secondWindowHandle = (String) iterator.next();
-        this.driver.switchTo().window(secondWindowHandle);
-        Assert.assertEquals(this.driver.getCurrentUrl(), "https://demoqa.com/");
+        driver.switchTo().window(secondWindowHandle);
+
+        Assert.assertEquals(driver.getCurrentUrl(), "https://demoqa.com/");
     }
 
     @Test
     public void checkDynamicLink() {
-        this.driver.get("https://demoqa.com/elements");
-        scrollToElement(this.driver, this.driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")));
-        this.driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")).click();
-        String actualURLButtons = this.driver.getCurrentUrl();
+        driver.get("https://demoqa.com/elements");
+        scrollToElement(driver, driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")));
+        driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")).click();
+        String actualURLButtons = driver.getCurrentUrl();
+
         Assert.assertEquals(actualURLButtons, "https://demoqa.com/links", "URL isn't correct");
-        this.driver.findElement(By.xpath("//a[@id='dynamicLink']")).click();
-        Set<String> windowHandles = this.driver.getWindowHandles();
+
+        driver.findElement(By.xpath("//a[@id='dynamicLink']")).click();
+        Set<String> windowHandles = driver.getWindowHandles();
         Iterator<String> iterator = windowHandles.iterator();
         String mainWindowHandle = (String) iterator.next();
         String secondWindowHandle = (String) iterator.next();
-        this.driver.switchTo().window(secondWindowHandle);
-        Assert.assertEquals(this.driver.getCurrentUrl(), "https://demoqa.com/");
+        driver.switchTo().window(secondWindowHandle);
+
+        Assert.assertEquals(driver.getCurrentUrl(), "https://demoqa.com/");
     }
 
     @Test
     public void checkCreatedApiCall() {
-        this.driver.get("https://demoqa.com/elements");
-        scrollToElement(this.driver, this.driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")));
-        this.driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")).click();
-        String actualURLButtons = this.driver.getCurrentUrl();
+        driver.get("https://demoqa.com/elements");
+        scrollToElement(driver, driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")));
+        driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")).click();
+        String actualURLButtons = driver.getCurrentUrl();
+
         Assert.assertEquals(actualURLButtons, "https://demoqa.com/links", "URL isn't correct");
-        this.driver.findElement(By.xpath("//a[@id='created']")).click();
-        scrollToElement(this.driver, this.driver.findElement(By.xpath("//a[@id='invalid-url']")));
-        WebElement resultLine = this.driver.findElement(By.xpath("//p[@id='linkResponse']"));
+
+        driver.findElement(By.xpath("//a[@id='created']")).click();
+        scrollToElement(driver, driver.findElement(By.xpath("//a[@id='invalid-url']")));
+        WebElement resultLine = driver.findElement(By.xpath("//p[@id='linkResponse']"));
+
         Assert.assertTrue(resultLine.getText().contains("201") && resultLine.getText().contains("Created"));
     }
 
     @Test
     public void checkNoContentApiCall() {
-        this.driver.get("https://demoqa.com/elements");
-        scrollToElement(this.driver, this.driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")));
-        this.driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")).click();
-        String actualURLButtons = this.driver.getCurrentUrl();
+        driver.get("https://demoqa.com/elements");
+        scrollToElement(driver, driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")));
+        driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")).click();
+        String actualURLButtons = driver.getCurrentUrl();
+
         Assert.assertEquals(actualURLButtons, "https://demoqa.com/links", "URL isn't correct");
-        this.driver.findElement(By.xpath("//a[@id='no-content']")).click();
-        scrollToElement(this.driver, this.driver.findElement(By.xpath("//a[@id='invalid-url']")));
-        WebElement resultLine = this.driver.findElement(By.xpath("//p[@id='linkResponse']"));
+
+        driver.findElement(By.xpath("//a[@id='no-content']")).click();
+        scrollToElement(driver, driver.findElement(By.xpath("//a[@id='invalid-url']")));
+        WebElement resultLine = driver.findElement(By.xpath("//p[@id='linkResponse']"));
+
         Assert.assertTrue(resultLine.getText().contains("204") && resultLine.getText().contains("No Content"));
     }
 
     @Test
     public void checkMovedApiCall() {
-        this.driver.get("https://demoqa.com/elements");
-        scrollToElement(this.driver, this.driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")));
-        this.driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")).click();
-        String actualURLButtons = this.driver.getCurrentUrl();
+        driver.get("https://demoqa.com/elements");
+        scrollToElement(driver, driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")));
+        driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")).click();
+        String actualURLButtons = driver.getCurrentUrl();
+
         Assert.assertEquals(actualURLButtons, "https://demoqa.com/links", "URL isn't correct");
-        this.driver.findElement(By.xpath("//a[@id='moved']")).click();
-        scrollToElement(this.driver, this.driver.findElement(By.xpath("//a[@id='invalid-url']")));
-        WebElement resultLine = this.driver.findElement(By.xpath("//p[@id='linkResponse']"));
+
+        driver.findElement(By.xpath("//a[@id='moved']")).click();
+        scrollToElement(driver, driver.findElement(By.xpath("//a[@id='invalid-url']")));
+        WebElement resultLine = driver.findElement(By.xpath("//p[@id='linkResponse']"));
+
         Assert.assertTrue(resultLine.getText().contains("301") && resultLine.getText().contains("Moved Permanently"));
     }
 
     @Test
     public void checkBadRequestApiCall() {
-        this.driver.get("https://demoqa.com/elements");
-        scrollToElement(this.driver, this.driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")));
-        this.driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")).click();
-        String actualURLButtons = this.driver.getCurrentUrl();
+        driver.get("https://demoqa.com/elements");
+        scrollToElement(driver, driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")));
+        driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")).click();
+        String actualURLButtons = driver.getCurrentUrl();
+
         Assert.assertEquals(actualURLButtons, "https://demoqa.com/links", "URL isn't correct");
-        this.driver.findElement(By.xpath("//a[@id='bad-request']")).click();
-        scrollToElement(this.driver, this.driver.findElement(By.xpath("//a[@id='invalid-url']")));
-        WebElement resultLine = this.driver.findElement(By.xpath("//p[@id='linkResponse']"));
+
+        driver.findElement(By.xpath("//a[@id='bad-request']")).click();
+        scrollToElement(driver, driver.findElement(By.xpath("//a[@id='invalid-url']")));
+        WebElement resultLine = driver.findElement(By.xpath("//p[@id='linkResponse']"));
+
         Assert.assertTrue(resultLine.getText().contains("400") && resultLine.getText().contains("Bad Request"));
     }
 
     @Test
     public void checkUnauthorizedApiCall() {
-        this.driver.get("https://demoqa.com/elements");
-        scrollToElement(this.driver, this.driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")));
-        this.driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")).click();
-        String actualURLButtons = this.driver.getCurrentUrl();
+        driver.get("https://demoqa.com/elements");
+        scrollToElement(driver, driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")));
+        driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")).click();
+        String actualURLButtons = driver.getCurrentUrl();
+
         Assert.assertEquals(actualURLButtons, "https://demoqa.com/links", "URL isn't correct");
-        this.driver.findElement(By.xpath("//a[@id='unauthorized']")).click();
-        scrollToElement(this.driver, this.driver.findElement(By.xpath("//a[@id='invalid-url']")));
-        WebElement resultLine = this.driver.findElement(By.xpath("//p[@id='linkResponse']"));
+
+        driver.findElement(By.xpath("//a[@id='unauthorized']")).click();
+        scrollToElement(driver, driver.findElement(By.xpath("//a[@id='invalid-url']")));
+        WebElement resultLine = driver.findElement(By.xpath("//p[@id='linkResponse']"));
+
         Assert.assertTrue(resultLine.getText().contains("401") && resultLine.getText().contains("Unauthorized"));
     }
 
     @Test
     public void checkForbiddenApiCall() {
-        this.driver.get("https://demoqa.com/elements");
-        scrollToElement(this.driver, this.driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")));
-        this.driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")).click();
-        String actualURLButtons = this.driver.getCurrentUrl();
+        driver.get("https://demoqa.com/elements");
+        scrollToElement(driver, driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")));
+        driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")).click();
+        String actualURLButtons = driver.getCurrentUrl();
+
         Assert.assertEquals(actualURLButtons, "https://demoqa.com/links", "URL isn't correct");
-        this.driver.findElement(By.xpath("//a[@id='forbidden']")).click();
-        scrollToElement(this.driver, this.driver.findElement(By.xpath("//a[@id='invalid-url']")));
-        WebElement resultLine = this.driver.findElement(By.xpath("//p[@id='linkResponse']"));
+
+        driver.findElement(By.xpath("//a[@id='forbidden']")).click();
+        scrollToElement(driver, driver.findElement(By.xpath("//a[@id='invalid-url']")));
+        WebElement resultLine = driver.findElement(By.xpath("//p[@id='linkResponse']"));
+
         Assert.assertTrue(resultLine.getText().contains("403") && resultLine.getText().contains("Forbidden"));
     }
 
     @Test
     public void checkNotFoundApiCall() {
-        this.driver.get("https://demoqa.com/elements");
-        scrollToElement(this.driver, this.driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")));
-        this.driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")).click();
-        String actualURLButtons = this.driver.getCurrentUrl();
+        driver.get("https://demoqa.com/elements");
+        scrollToElement(driver, driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")));
+        driver.findElement(By.xpath("//li[@id='item-5']//span[text()='Links']")).click();
+        String actualURLButtons = driver.getCurrentUrl();
+
         Assert.assertEquals(actualURLButtons, "https://demoqa.com/links", "URL isn't correct");
-        scrollToElement(this.driver, this.driver.findElement(By.xpath("//a[@id='invalid-url']")));
-        this.driver.findElement(By.xpath("//a[@id='invalid-url']")).click();
-        WebElement resultLine = this.driver.findElement(By.xpath("//p[@id='linkResponse']"));
+
+        scrollToElement(driver, driver.findElement(By.xpath("//a[@id='invalid-url']")));
+        driver.findElement(By.xpath("//a[@id='invalid-url']")).click();
+        WebElement resultLine = driver.findElement(By.xpath("//p[@id='linkResponse']"));
+
         Assert.assertTrue(resultLine.getText().contains("404") && resultLine.getText().contains("Not Found"));
     }
 }
